893cfc8f88f84fa5f3205561a727fcc89f2f0a22
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-27 21:47:37 +0530

    [Ritvik]Add: Count of remaining Chapters

--- CollectionProblem/Program.cs
+++ CollectionProblem/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Collection Practice Probloms!!!");
+            Console.WriteLine("Welcome to Collection Practice Probloms!!!\n");
+
+            int n = 4;
+            int k = 7;
+
+            Dictionary<int,int> chapters = new Dictionary<int,int>();
+
+            chapters.Add(1, 4);
+            chapters.Add(5, 7);
+            chapters.Add(9, 16);
+            chapters.Add(17, 26);
+
+            int chaptersToRead = ChaptersCount.ChaptersToRead(n, chapters, k);
+            Console.WriteLine("The Number of Chapters to Read: "+chaptersToRead);
+
         }
     }
 }
