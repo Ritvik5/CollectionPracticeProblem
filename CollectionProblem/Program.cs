7041a0adb3103243d30abe78018856a5fb0a7e09
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-27 21:36:44 +0530

    [Ritvik]Add: Welcome Message

---
+++ CollectionProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Collection Practice Probloms!!!");
+        }
+    }
+}
